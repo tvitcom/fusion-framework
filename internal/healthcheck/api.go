7449265d8fca22ca77ec18715de911a9fb24b349
name <me@my.lancome>
2021-09-16 22:25:11 +0300

    metrics: add loadavg responce on healthcheking request

--- internal/healthcheck/api.go
+++ internal/healthcheck/api.go
@@ -1,6 +1,8 @@
 package healthcheck
 
 import (
+	"os/exec"
+	"log"
 	// routing "github.com/go-ozzo/ozzo-routing/v2"
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,10 +12,16 @@ var appver string
 // RegisterHandlers registers the handlers that perform healthchecks.
 func RegisterHandlers(r *fiber.App, ver string) {
 	appver = ver
-	r.Get("/healthcheck", check)
+	r.Get("/healthcheck", healthchecking)
 }
 
 // healthcheck responds to a healthcheck request.
-func check(c *fiber.Ctx) error {
-	return c.SendString("OK " + appver)
+func healthchecking(c *fiber.Ctx) error {
+	// Exec: cat /proc/loadavg
+	lsCmd := exec.Command("cat", "/proc/loadavg")
+	lsOut, err := lsCmd.Output()
+	if err != nil {
+		log.Fatal("Loadavg command dont respond")
+	}
+	return c.SendString("OK " + string(lsOut))
 }
